3482afa2b21e2da6ece86c3ecc95d928427a621c
mabrouki23 <mabrouki23@hotmail.fr>
2022-12-25 16:06:28 -0500

    commit

--- demo/src/main/java/com/example/demo/controller.java
+++ demo/src/main/java/com/example/demo/controller.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api")
+
 public class controller {
-	@RequestMapping("/index")
+	@RequestMapping("/")
 	public String index() {
 		return "hello from spring boot";
 	}
